ae40501a26b6a4cf5d312980a206a75da200bed4
rahulmarslama <rahulrlama@miu.edu>
2026-03-09 16:42:06 -0500

    Problem 3

--- src/Assignment6/prob3/Order.java
+++ src/Assignment6/prob3/Order.java
@@ -1,5 +1,7 @@
 package Assignment6.prob3;
 
+import java.util.Objects;
+
 public class Order {
     private String orderId;
     private String customerName;
@@ -12,17 +14,23 @@ public class Order {
     }
 
     // getters/setters
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
 
     @Override
     public boolean equals(Object o) {
-        // implement: same orderId => equal
-        return false;
+        if (!(o instanceof Order order)) return false;
+        return Objects.equals(orderId, order.orderId);
     }
 
     @Override
     public int hashCode() {
-        // consistent with equals
-        return 0;
+        return Objects.hashCode(orderId);
     }
 
     @Override
--- src/Assignment6/prob3/OrderTest.java
+++ src/Assignment6/prob3/OrderTest.java
@@ -3,12 +3,62 @@ package Assignment6.prob3;
 import java.util.*;
 
 public class OrderTest {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // a) through i) here, print after each step (use for-each)
+        ArrayList<Order> orderList = new ArrayList<>();
+        Order o1 = new Order("1234","Rahul Lama",44.23);
+        Order o2 = (new Order("5231","Bikram Rokka",243244.20));
+        Order o3 = (new Order("7659","Priya Dutta",1223.36));
+        Order o4 = (new Order("2314","Abhisek Bachan",423255.23));
+        Order o5 = (new Order("3802","Sharukh Khan",42434.23));
+        orderList.add(o1);
+        orderList.add(o2);
+        orderList.add(o3);
+        orderList.add(o4);
+        orderList.add(o5);
+
+        System.out.println(orderList.size());
+        System.out.println(orderList);
+        orderList.remove(o2);
+        System.out.println(orderList.size());
+        System.out.println(orderList);
+
+        System.out.println("The order at position 1 is: "+orderList.get(1));
+
+        Order newOrder = new Order("1244","Dylan Panta",12321.21);
+        orderList.set(1,newOrder);
+        System.out.println("The order at position 1 is: "+orderList.get(1));
+
+        orderList.sort(new Comparator<Order>() {
+            @Override
+            public int compare(Order o1, Order o2) {
+                return Double.compare(o1.getTotalAmount(),o2.getTotalAmount());
+            }
+        });
+        System.out.println(orderList);
+
+        List<Order> filteredOrder = listMoreThan50(orderList);
+        System.out.println("The list with more than 50 is: ");
+        filteredOrder.sort(new Comparator<Order>() {
+            @Override
+            public int compare(Order o1, Order o2) {
+                return o1.getCustomerName().compareTo(o2.getCustomerName());
+            }
+        });
+        System.out.println(filteredOrder);
     }
 
     public static List<Order> listMoreThan50(List<Order> list) {
         // return new ArrayList<>(...) with totalAmount > 50.0
-        return null;
+        List<Order> returnList = new ArrayList<>();
+        for (Order order: list)
+        {
+            if(order.getTotalAmount() > 50)
+            {
+                returnList.add(order);
+            }
+        }
+        return returnList;
     }
 }
