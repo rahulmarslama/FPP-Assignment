8df62ce5da900249867be9e7c896a8e778a2e67a
rahulmarslama <rahulrlama@miu.edu>
2026-03-11 17:32:34 -0500

    Problem 1

--- src/Assignment7/Problem1/ArrayQueueImpl.java
+++ src/Assignment7/Problem1/ArrayQueueImpl.java
@@ -1,44 +1,119 @@
 package Assignment7.Problem1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ArrayQueueImpl
 {
     private final int DEFAULT_CAPACITY = 10;
     private int size;
+    public int capacity;
     private Integer[] arr;
-    private int front;
-    private int rear;
+    public int front;
+    public int rear;
+
+    public ArrayQueueImpl()
+    {
+        arr= new Integer[DEFAULT_CAPACITY];
+        front=-1;
+        rear=-1;
+        size=0;
+        capacity = DEFAULT_CAPACITY;
+    }
     // Add Constructors, Default and Parameterized to initialize instance fields
+
     public Integer peek()
     {
-        return null;
-    //implement
+        if(isEmpty()) return null;
+        return arr[front];
     }
     public void enqueue(int obj)
     {
-    //implement
+        if((front-rear-1)% capacity == 0)
+        {
+            resize();
+        }
+        if(isEmpty())
+        {
+            front = (front+1) % capacity;
+            arr[front]=obj;
+            rear= (rear+1)%capacity;
+        }
+        else
+        {
+            rear = (rear+1)%capacity;
+            arr[rear] = obj;
+        }
+        ++size;
+        /*if((front+rear+1)%size == 0)
+        {
+            resize();
+            //to be implemented
+        }*/
     }
     public Integer dequeue()
     {
-        return null;
+        if(isEmpty()) return -1;
+        int deletedValue = arr[front];
+        /*if(front == rear)
+        {
+            rear = -1;
+            front =-1;
+        }*/
+        front = (front+1)%capacity;
+        --size;
+        return deletedValue;
     }
     public boolean isEmpty()
     {
-    //implement
-        return false;
+        return size==0;
     }
     public int size()
     {
-    //implement
-        return 0;
+        return size;
     }
-    private void resize()
+    public void resize()
     {
-    //implement
+        if(isEmpty()) return;
+        /*if((front-rear-1)% capacity == 0)
+        {
+            return;
+        }*/
+        Integer[] temp = new Integer[arr.length*2];
+
+        int index =front;
+        int runningIndex = (index+1)%capacity;
+
+        temp[0] = arr[front];
+        int i=1;
+        while(runningIndex!=rear)
+        {
+            temp[i] = arr[runningIndex];
+            i++;
+            runningIndex = (runningIndex+1)%capacity;
+        }
+        temp[i] = arr[rear];
+
+        arr = temp;
+        front = 0;
+        rear = size-1;
+        capacity = temp.length;
     }
     public String toString()
     {
+        StringBuilder str = new StringBuilder("[");
+        int index = front;
+        for(int i=0;i<size;i++)
+        {
+            str.append(arr[index]);
+            str.append(" ");
+            index = (index+1) % capacity;
+        }
+        str.append("]");
     /* Return data in this format, each element separated by comma with in [ ] eg:
     [10, 20, 30, 40, 50, 60 ]*/
-        return null;
+        return str.toString();
     }
 }
--- src/Assignment7/Problem1/ArrayQueueImplTest.java
+++ src/Assignment7/Problem1/ArrayQueueImplTest.java
@@ -1,9 +1,111 @@
 package Assignment7.Problem1;
 
+import java.util.Queue;
+
 public class ArrayQueueImplTest
 {
     static void main()
     {
+        ArrayQueueImpl cirQueue = new ArrayQueueImpl();
+
+        System.out.println("Size : "+cirQueue.size());
+        System.out.println("Is Empty: "+cirQueue.isEmpty());
+        System.out.println("First Element : "+ cirQueue.peek());
+        System.out.println(cirQueue.toString());
+
+        cirQueue.enqueue(1);
+        System.out.println("Size : "+cirQueue.size());
+        System.out.println("Is Empty: "+cirQueue.isEmpty());
+        System.out.println("First Element : "+ cirQueue.peek());
+        System.out.println(cirQueue.toString());
+
+        cirQueue.enqueue(2);
+        cirQueue.enqueue(3);
+        cirQueue.enqueue(4);
+        cirQueue.enqueue(5);
+        cirQueue.enqueue(6);
+        cirQueue.enqueue(7);
+        cirQueue.enqueue(8);
+        cirQueue.enqueue(9);
+        cirQueue.enqueue(10);
+
+        System.out.println("Size : "+cirQueue.size());
+        System.out.println("Is Empty: "+cirQueue.isEmpty());
+        System.out.println("First Element : "+ cirQueue.peek());
+        System.out.println(cirQueue.toString());
+
+        System.out.println(cirQueue.dequeue());
+        System.out.println(cirQueue.dequeue());
+        System.out.println(cirQueue.dequeue());
+        System.out.println(cirQueue.dequeue());
+        System.out.println(cirQueue.dequeue());
+        System.out.println(cirQueue.dequeue());
+        System.out.println(cirQueue.dequeue());
+        System.out.println(cirQueue.dequeue());
+        System.out.println(cirQueue.dequeue());
+        System.out.println("Size : "+cirQueue.size());
+        System.out.println("Is Empty: "+cirQueue.isEmpty());
+        System.out.println("First Element : "+ cirQueue.peek());
+        System.out.println("front: "+ cirQueue.front);
+        System.out.println("rear: "+ cirQueue.rear);
+        System.out.println(cirQueue.toString());
+
+        cirQueue.enqueue(1);
+        System.out.println("Size : "+cirQueue.size());
+        System.out.println("Is Empty: "+cirQueue.isEmpty());
+        System.out.println("First Element : "+ cirQueue.peek());
+        System.out.println(cirQueue.toString());
+        System.out.println("Capacity: "+ cirQueue.capacity);
+        System.out.println("front: "+ cirQueue.front);
+        System.out.println("rear: "+ cirQueue.rear);
+
+
+        cirQueue.resize();
+        System.out.println("Capacity: "+ cirQueue.capacity);
+        System.out.println("front: "+ cirQueue.front);
+        System.out.println("rear: "+ cirQueue.rear);
+        System.out.println(cirQueue.toString());
+
+        cirQueue.dequeue();
+        cirQueue.dequeue();
+        cirQueue.dequeue();
+        cirQueue.dequeue();
+        cirQueue.dequeue();
+        cirQueue.dequeue();
+        System.out.println("Capacity: "+ cirQueue.capacity);
+        System.out.println("front: "+ cirQueue.front);
+        System.out.println("rear: "+ cirQueue.rear);
+        System.out.println(cirQueue.toString());
+
+
+        cirQueue.enqueue(2);
+        cirQueue.enqueue(3);
+        cirQueue.enqueue(4);
+        cirQueue.enqueue(5);
+        cirQueue.enqueue(6);
+        cirQueue.enqueue(7);
+        cirQueue.enqueue(8);
+        cirQueue.enqueue(9);
+        cirQueue.enqueue(10);
+        cirQueue.enqueue(2);
+        cirQueue.enqueue(3);
+        cirQueue.enqueue(4);
+        cirQueue.enqueue(5);
+        cirQueue.enqueue(6);
+        cirQueue.enqueue(7);
+        cirQueue.enqueue(8);
+        cirQueue.enqueue(8);
+        cirQueue.enqueue(8);
+        cirQueue.enqueue(8);
+//        cirQueue.enqueue(9);
+//        cirQueue.enqueue(10);
+
+        System.out.println("Capacity: "+ cirQueue.capacity);
+        System.out.println("front: "+ cirQueue.front);
+        System.out.println("rear: "+ cirQueue.rear);
+        System.out.println(cirQueue.toString());
+
+
 
     }
 }
