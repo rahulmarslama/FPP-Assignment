3dce3ec6c7478100a581daa4ddfed5d919130ba4
rahulmarslama <rahulrlama@miu.edu>
2026-03-11 14:15:45 -0500

    Assignment7 init

---
+++ src/Assignment7/Problem1/ArrayQueueImpl.java
@@ -0,0 +1,44 @@
+package Assignment7.Problem1;
+
+public class ArrayQueueImpl
+{
+    private final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private Integer[] arr;
+    private int front;
+    private int rear;
+    // Add Constructors, Default and Parameterized to initialize instance fields
+    public Integer peek()
+    {
+        return null;
+    //implement
+    }
+    public void enqueue(int obj)
+    {
+    //implement
+    }
+    public Integer dequeue()
+    {
+        return null;
+    }
+    public boolean isEmpty()
+    {
+    //implement
+        return false;
+    }
+    public int size()
+    {
+    //implement
+        return 0;
+    }
+    private void resize()
+    {
+    //implement
+    }
+    public String toString()
+    {
+    /* Return data in this format, each element separated by comma with in [ ] eg:
+    [10, 20, 30, 40, 50, 60 ]*/
+        return null;
+    }
+}
---
+++ src/Assignment7/Problem1/ArrayQueueImplTest.java
@@ -0,0 +1,9 @@
+package Assignment7.Problem1;
+
+public class ArrayQueueImplTest
+{
+    static void main()
+    {
+
+    }
+}
